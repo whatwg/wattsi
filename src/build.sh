e8102f1cc843d594563fd9b1226773322ae89628
Michael[tm] Smith <mike@w3.org>
2017-04-19 04:56:00 +0900

    Add --version option
    Relates to https://github.com/whatwg/html-build/issues/84.

--- src/build.sh
+++ src/build.sh
@@ -8,7 +8,18 @@ SRC=""
 #MODE="MEMCHECK"
 MODE="RELEASE"
 
+get_abs_filename() {
+  # We need to get absolute paths because different parts of this script
+  # manipulate files from different directories.
+  echo "$(cd "$(dirname "$1")" || exit; pwd)/$(basename "$1")"
+}
+
+VERSION_FILE="$(get_abs_filename version.inc)"
 PATHS="-Fu${SRC}html -Fi${SRC}html -Fi${SRC}html/entities.inc"
 DEFINES="-dUSEROPES -dLINES -dPARSEERROR"
 
+echo "Writing $VERSION_FILE"
+git rev-list --count HEAD > "$VERSION_FILE"
 . ${SRC}lib/compile.sh
+echo "Removing $VERSION_FILE"
+rm "$VERSION_FILE"
