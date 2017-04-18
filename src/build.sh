8fd56d0de7d00b18258e08abd88e35dd3ef1c664
Michael[tm] Smith <mike@w3.org>
2017-04-18 15:21:36 +0900

    Make get_abs_filename() and use it

--- src/build.sh
+++ src/build.sh
@@ -7,8 +7,14 @@ SRC=""
 #MODE="PROFILE"
 #MODE="MEMCHECK"
 MODE="RELEASE"
-VERSION_FILE="$(cd "$(dirname "$1")" || exit; pwd)/$(basename version.inc)"
 
+get_abs_filename() {
+  # We need to get absolute paths because different parts of this script
+  # manipulate files from different directories.
+  echo "$(cd "$(dirname "$1")" || exit; pwd)/$(basename "$1")"
+}
+
+VERSION_FILE="$(get_abs_filename version.inc)"
 PATHS="-Fu${SRC}html -Fi${SRC}html -Fi${SRC}html/entities.inc"
 DEFINES="-dUSEROPES -dLINES -dPARSEERROR"
 
