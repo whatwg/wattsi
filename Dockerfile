2a6cbf4f35e4231954604ed2ae891e36af4d940f
Domenic Denicola <d@domenic.me>
2020-05-21 18:01:27 -0400

    Build and deploy Wattsi on CI using Docker
    This adds a Dockerfile which, when built, contains the Wattsi executable (with debian:stable-slim as the base). This effectively provides pre-built Wattsi binaries, uploaded to the Docker hub, for those with Docker available. It also gives us CI coverage for Wattsi, which we were lacking.
    
    This closes #192 by removing the hard-coded version. With this in place the HTML build infrastructure should no longer need that.
    
    Along the way, this adds shebangs to various .sh files (required in at least some OSes, including the Debian one we're using in Docker), and removes the test commands from the compile.sh script (those can be run separately, after building).

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM debian:stable-slim
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends fp-compiler fp-units-fcl fp-units-net libc6-dev
+
+COPY src /whatwg/wattsi/src
+RUN mkdir -p /whatwg/wattsi/bin
+
+WORKDIR /whatwg/wattsi/src
+RUN ./build.sh
+
+RUN rm -rf /whatwg/wattsi/src
+RUN apt-get purge -y fp-compiler fp-units-fcl fp-units-net && \
+    apt-get autoremove -y
+
+ENTRYPOINT ["/whatwg/wattsi/bin/wattsi"]
+CMD ["--version"]
--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 MAIN="wattsi"
 SRC=""
 #MODE="DEBUG"
@@ -18,10 +20,16 @@ VERSION_FILE="$(get_abs_filename version.inc)"
 PATHS="-Fu${SRC}html -Fi${SRC}html -Fi${SRC}html/entities.inc"
 DEFINES="-dUSEROPES -dLINES -dPARSEERROR"
 
-echo "Writing $VERSION_FILE"
-# If you update the fallback below also update WATTSI_LATEST in
-# https://github.com/whatwg/html-build/blob/master/build.sh
-(git rev-list --count HEAD || echo "91") > "$VERSION_FILE"
+if [[ -x "$(command -v git)" ]]; then
+  echo "Writing $VERSION_FILE"
+  # If you update the fallback below also update WATTSI_LATEST in
+  # https://github.com/whatwg/html-build/blob/master/build.sh
+  git rev-list --count HEAD > "$VERSION_FILE"
+elif [[ -f "$VERSION_FILE" ]]; then
+  echo "$VERSION_FILE exists: version $(cat $VERSION_FILE)"
+else
+  echo "$VERSION_FILE must exist, or git must be installed so we can infer it"
+  exit 1
+fi
+
 . ${SRC}lib/compile.sh
-echo "Removing $VERSION_FILE"
-rm "$VERSION_FILE"
